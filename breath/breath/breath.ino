78053a6637de7ee4549da57ed458da48a9ba6b3d
Zeken <eszeken@hotmail.com>
2015-11-21 20:46:17 +0100

    Change breath for proper usage of MTBASerial

--- breath/breath/breath.ino
+++ breath/breath/breath.ino
@@ -11,18 +11,10 @@ MTBASerial serial;
 
 void setup() {
   serial.begin();
-  serial.send(123);
-  delay(100);
 }
 
 void loop() {
   breathSignal = analogRead(breathPin);
   
-  byte msbBreathSignal = breathSignal>>8;  
-  byte lsbBreathSignal = breathSignal&255;
-  
-  serial.send(msbBreathSignal);
-  serial.send(lsbBreathSignal);
-//  Serial.write(breathSignal);
-//  Serial.println(breathSignal);  
+  serial.send(breathSignal);
 }
