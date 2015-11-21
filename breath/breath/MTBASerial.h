f1aa0cc73c289a670bdb5c3a796126b878c812e8
Zeken <eszeken@hotmail.com>
2015-11-21 21:01:38 +0100

    Added documentation to MTBASerial.h

--- breath/breath/MTBASerial.h
+++ breath/breath/MTBASerial.h
@@ -1,3 +1,24 @@
+/*
+  MTBASerial.h - Library for reliable serial communication on MTBA.
+
+  A simple communication protocol is agreed between Arduino and
+  the client:
+
+    255 - channel - length - data
+
+  - "channel" must be < 5
+  - "length" is expressed in bytes and must be < 5. It represents
+  the number of bytes in "data".
+  - "data" consists on one or more bytes representing the value
+  to be transmitted. For values bigger than 1 byte, the value
+  is split into multiple single bytes that are transmitted
+  sequentially from MSB to LSB. The client should then regroup
+  the received bytes to get the original value.
+
+  Created by Eloi Marin Gratacos (eszeken@hotmail.com)
+  Released into the public domain.
+*/
+
 #ifndef MTBASerial_h
 #define MTBASerial_h
 
