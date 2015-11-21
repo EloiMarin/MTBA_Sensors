652c41433da764727ec9145f70771549a4db3a98
Zeken <eszeken@hotmail.com>
2015-11-21 20:18:35 +0100

    Added channel to MTBASerial

--- breath/breath/MTBASerial.cpp
+++ breath/breath/MTBASerial.cpp
@@ -1,19 +1,29 @@
 #include "MTBASerial.h"
-#include "Arduino.h"
 
 void MTBASerial::begin()
 {
   Serial.begin(_baud);
 }
 
-void MTBASerial::write(char data)
+void MTBASerial::write(byte data, int channel)
 {
+  if (!isValidChannel(channel))
+    return;
 }
 
-void MTBASerial::write(int data)
+void MTBASerial::write(int data, int channel)
 {
+  if (!isValidChannel(channel))
+    return;
 }
 
-void MTBASerial::write(long data)
+void MTBASerial::write(long data, int channel)
 {
+  if (!isValidChannel(channel))
+    return;
+}
+
+boolean MTBASerial::isValidChannel(int channel)
+{
+  return channel < _maxChannels;
 }
--- breath/breath/MTBASerial.h
+++ breath/breath/MTBASerial.h
@@ -1,17 +1,22 @@
 #ifndef MTBASerial_h
 #define MTBASerial_h
 
+#include "Arduino.h"
+
 class MTBASerial
 {
   public:
     void begin();
     
-    void write(char);
-    void write(int);
-    void write(long);
+    void write(byte data, int channel = 0);
+    void write(int data, int channel = 0);
+    void write(long data, int channel = 0);
     
   private:
     const int _baud = 14400;
+    const int _maxChannels = 5;
+
+    boolean isValidChannel(int channel);
 };
 
 #endif // MTBASerial_h
