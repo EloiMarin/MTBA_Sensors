23ebe6132cb7cc5865c778ef8277585b242d4881
Zeken <eszeken@hotmail.com>
2015-11-21 23:52:47 +0100

    Made breathSignal a local variable

--- breath/breath/breath.ino
+++ breath/breath/breath.ino
@@ -5,7 +5,6 @@
 #include "MTBASerial.h"
 
 int breathPin = A2;
-int breathSignal = 0;
 
 MTBASerial serial;
 
@@ -14,7 +13,7 @@ void setup() {
 }
 
 void loop() {
-  breathSignal = analogRead(breathPin);
+  int breathSignal = analogRead(breathPin);
   serial.send(breathSignal);
   
   delay(16);
