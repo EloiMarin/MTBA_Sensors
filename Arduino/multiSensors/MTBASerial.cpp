e5f0543e00af62d09b766b601ad2b7003bdecf8f
Eloi Marin Gratacos <eszeken@hotmail.com>
2015-11-22 19:55:18 +0100

    Added multiSensors Arduino and PureData projects

---
+++ Arduino/multiSensors/MTBASerial.cpp
@@ -0,0 +1,52 @@
+#include "MTBASerial.h"
+
+void MTBASerial::begin()
+{
+  Serial.begin(_baud);
+}
+
+void MTBASerial::send(byte data, word channel)
+{
+  if (!isValidChannel(channel))
+    return;
+  
+  byte sendBuffer[4] = { 255, channel, 1, data };
+  Serial.write(sendBuffer, sizeof(sendBuffer));
+}
+
+void MTBASerial::send(int data, word channel)
+{
+  if (!isValidChannel(channel))
+    return;
+
+  if (_isInt32b)
+  {
+    send(static_cast<long>(data), channel);
+    return;
+  }
+
+  byte dataMSB = data >> 8;
+  byte dataLSB = data & 255;
+
+  byte sendBuffer[5] = { 255, channel, 2, dataMSB, dataLSB };
+  Serial.write(sendBuffer, sizeof(sendBuffer));
+}
+
+void MTBASerial::send(long data, word channel)
+{
+  if (!isValidChannel(channel))
+    return;
+
+  byte dataB24 = data >> 24;
+  byte dataB16 = data >> 16;
+  byte dataB8 = data >> 8;
+  byte dataB0 = data & 255;
+
+  byte sendBuffer[7] = { 255, channel, 4, dataB24, dataB16, dataB8, dataB0 };
+  Serial.write(sendBuffer, sizeof(sendBuffer));
+}
+
+boolean MTBASerial::isValidChannel(word channel)
+{
+  return channel < _maxChannels;
+}
